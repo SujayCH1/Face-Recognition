ae466e9ca55dcb8778ff394d91ab711bdbd9e911
SujayCH1 <145866622+SujayCH1@users.noreply.github.com>
2024-03-02 19:59:10 +0530

    Update capture_images.py

--- capture_images.py
+++ capture_images.py
@@ -3,9 +3,9 @@ import os
 import json
 
 def capture_images(output_dir, num_images_per_person):
-    current_dir = os.path.dirname(os.path.abspath(__file__))  # Get the current directory of the script
-    images_folder = os.path.join(current_dir, output_dir)  # Set the images folder path inside the current directory
-    json_file = os.path.join(current_dir, 'labels_images_mapping.json')  # Path to the JSON file
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    images_folder = os.path.join(current_dir, output_dir)
+    json_file = os.path.join(current_dir, 'labels_images_mapping.json')
 
     if not os.path.exists(images_folder):
         os.makedirs(images_folder)
@@ -51,7 +51,7 @@ def capture_images(output_dir, num_images_per_person):
                 break
 
         image_paths = [os.path.abspath(os.path.join(person_folder, image)) for image in os.listdir(person_folder)]
-        labels_images_mapping[label] = [path.replace('\\\\', '\\') for path in image_paths]  # Preprocess file paths
+        labels_images_mapping[label] = [path.replace('\\\\', '\\') for path in image_paths]
 
     cap.release()
     cv2.destroyAllWindows()
@@ -61,7 +61,7 @@ def capture_images(output_dir, num_images_per_person):
 
 def main():
     output_dir = 'captured_images'
-    num_images_per_person = 100  # Adjust as needed
+    num_images_per_person = 100
     capture_images(output_dir, num_images_per_person)
 
 if __name__ == "__main__":
